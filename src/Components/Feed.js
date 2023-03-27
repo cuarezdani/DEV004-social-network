aa99b3c2e684b0d3093518a9dd72391f16d2dfc6
Tiare Rojas Madariaga <tiarerojasmadariaga_@hotmail.com>
2023-03-27 10:36:32 -0300

    boton like

--- src/Components/Feed.js
+++ src/Components/Feed.js
@@ -1,4 +1,4 @@
-import { onNavigate } from '../router';
+// import { onNavigate } from '../router';
 
 export const Feed = () => {
   const containerFeed = document.createElement('div');
@@ -22,13 +22,23 @@ export const Feed = () => {
   fotoMuro.className = 'fotoMuro';
   fotoMuro.src = '../imagenes/muro1.png';
 
+  // cree section para que se contengan los comentarios y queden  con el otro section
+  const sectionComments = document.createElement('section');
+  sectionComments.className = 'sectionComments';
+  const inputComments = document.createElement('input'); // input para que escriban
+  inputComments.type = 'text';
+  inputComments.placeholder = 'Leave a comment';
+  // pero se neceista llamar o dejar el comentario hacer boton?
+
   // section general, span corazon+button, span favorito estrella+button,
   // span comentarios+button, span guardar+
   const sectionIconos = document.createElement('section');
   sectionIconos.className = 'sectionIconos';
   const like = document.createElement('img');
   like.className = 'like';
+  like.type = 'button';
   like.src = '../imagenes/like.png';
+  like.id = 'clickbtn';
   const favorite = document.createElement('img');
   favorite.className = 'favorite';
   favorite.src = '../imagenes/favorite.png';
@@ -53,13 +63,36 @@ export const Feed = () => {
   comentario.className = 'comentario;
   comentario.textContent = ' */
 
+  // se crea una variable para que se guarden los numeros de los likes
+  // se crea un span el cual es un elemtno en linea que solo toma el espacio que se le da
+  const likeContador = document.createElement('span');
+  likeContador.className = 'likes';
+  let likes = 0; // comienza el contador con el 0
+  likeContador.textContent = likes; // nos deberia dar lo escrito del like
+
+  // se usa handle para los eventos del boton dando funcionalidades en el metodo click
+  function handleLikeClick() {
+    // eslint-disable-next-line no-plusplus
+    likes++;
+    likeContador.textContent = likes;
+  }
+  // se llama al like para qeu sea escuchado en el dom
+  like.addEventListener('click', handleLikeClick);
+
+  // like.getElementsById('clickbtn');
+  //  function printClick() {
+  //  console.log('hiciste click'); }
+  // like.addEventListener('click', printClick);
+
   containerFeed.appendChild(logoFeed);
   containerFeed.appendChild(imgWonderland);
   containerFeed.appendChild(section);
   section.append(imgWonderland, strong);
   containerFeed.appendChild(fotoMuro);
   containerFeed.appendChild(sectionIconos);
-  sectionIconos.append(like, favorite, comment, save);
+  sectionIconos.append(like, favorite, comment, save, likeContador);
+  containerFeed.appendChild(sectionComments);
+  sectionComments.append(inputComments);
 
   return containerFeed;
 };
